f8c902ac8a56b86c53772b44f92ed34603a31e36
Hafiz <hmstech@gmail.com>
2024-03-03 20:33:26 +0500

    initial commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='flex'>
+    <div className='logo'>
+        <Image src='' />
+    </div>
+    
+    </div>
+  )
+}

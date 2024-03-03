0ac77f33c724c1fa1692eaf550e74678ef8accdb
Hafiz <hmstech@gmail.com>
2024-03-03 21:04:44 +0500

    navbar desktop done

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,13 +1,36 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function Navbar() {
   return (
-    <div className='flex'>
-    <div className='logo'>
-        <Image src='' />
+    <div className="menu flex justify-between">
+      <div className="logo">
+        <Image src="" />
+      </div>
+      <div className="">
+        <ul className="menu-ul flex justify-between">
+          <li className="ml-[20px]">
+            <a href="#">Home</a>
+          </li>
+          <li className="ml-[20px]">
+            <a href="#">About</a>
+          </li>
+          <li className="ml-[20px]">
+            <a href="#">Music</a>
+          </li>
+          <li className="ml-[20px]">
+            <a href="#">Credits</a>
+          </li>
+          <li className="ml-[20px]">
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className="action-button flex justify-between gap-3">
+      <button className="primary-btn">Login</button>
+      <button className="secondary-btn">Singup</button>
+
+      </div>
     </div>
-    
-    </div>
-  )
+  );
 }
